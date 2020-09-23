934eedfa519553f265000dab04031769507e4925
Sekhar Ravinutala <sekhar@allurefx.com>
2020-09-22 20:15:30 -0700

    Added 9/22

---
+++ 09-22-2020/color1.cpp
@@ -0,0 +1,12 @@
+/**
+ * Main application that uses the Color class.
+ * 
+ * Copyright (c) 2020, Sekhar Ravinutala.
+ */
+
+#include "color.h"
+
+int main() {
+  Color c1(0.5, -1, 2);
+  dump(c1);  // (0.50, 0.00, 1.00)
+}
